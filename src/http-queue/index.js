41adb2186fdbdb541f5a0b0eea43811437c5224c
Jef King <jefkin@microsoft.com>
2017-06-29 14:25:39 -0700

    fix binding

--- src/http-queue/index.js
+++ src/http-queue/index.js
@@ -1,13 +1,13 @@
 module.exports = function (context, req) {
 
-    context.model = (typeof req.body != 'undefined' && typeof req.body == 'object') ? req.body : null;
-
-    let error = !context.model ? "no data; or invalid payload in body" : null;
+    let model = (typeof req.body != 'undefined' && typeof req.body == 'object') ? req.body : null;
+    let error = !model ? "no data; or invalid payload in body" : null;
 
     context.res = {
         status: error ? 500 : 200,
         body: error
     };
 
-    context.done(error, context.model);
+    context.bindings.out = model;
+    context.done(error);
 };
