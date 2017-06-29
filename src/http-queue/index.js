e202487419d1a48da83845fe0756db67343c4ab0
Jef King <jefkin@microsoft.com>
2017-06-29 13:28:16 -0700

    more HTTP goodness

--- src/http-queue/index.js
+++ src/http-queue/index.js
@@ -1,6 +1,11 @@
 module.exports = function (context, req) {
     let message = (typeof req.body != 'undefined' && typeof req.body == 'object') ? req.body : null;
-    let error = message == null ? "no data; or invald payload in body" : null;
+    let error = !message ? "no data; or invald payload in body" : null;
+
+    context.res = {
+        status: error ? 500 : 200,
+        body: error
+    };
 
     context.done(error, message);
 };
