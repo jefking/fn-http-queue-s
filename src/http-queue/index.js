cee3fdfdc92d57c850c146939e41e82599e72776
Jef King <jefkin@microsoft.com>
2017-06-29 13:41:13 -0700

    output binding

--- src/http-queue/index.js
+++ src/http-queue/index.js
@@ -2,7 +2,7 @@ module.exports = function (context, req) {
     
     context.model = (typeof req.body != 'undefined' && typeof req.body == 'object') ? req.body : null;
 
-    let error = !message ? "no data; or invald payload in body" : null;
+    let error = !context.model ? "no data; or invald payload in body" : null;
 
     context.res = {
         status: error ? 500 : 200,
