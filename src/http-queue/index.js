b56b3947e43eaccac93b021e09470d7c1e3bb7ab
Jef King <jefkin@microsoft.com>
2017-06-29 13:48:20 -0700

    logging

--- src/http-queue/index.js
+++ src/http-queue/index.js
@@ -1,8 +1,12 @@
 module.exports = function (context, req) {
-    
+
     context.model = (typeof req.body != 'undefined' && typeof req.body == 'object') ? req.body : null;
 
     let error = !context.model ? "no data; or invalid payload in body" : null;
+
+    context.log(req.body);
+    context.log('-------------------');
+    context.log(context.model);
 
     context.res = {
         status: error ? 500 : 200,
